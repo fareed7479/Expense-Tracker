b9b1503d824102d8563fe4b79e801fc5a9c324f7
fareed7479 <fareed.shaik@sasi.ac.in>
2025-04-02 10:17:32 +0530

    dep_new

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://expense-tracker-back-five.vercel.app";
+export const BASE_URL = "https://expense-tracker-beta-red-62.vercel.app/";
 // export const BASE_URL = "http://localhost:8000";
 
 
